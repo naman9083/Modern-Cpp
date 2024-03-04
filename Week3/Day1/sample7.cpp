7984bb248028e2109fe42c167c8b7a15ec04b6b9
naman9083 <namankhurana9083@gmail.com>
2024-03-04 09:22:52 +0530

    Week3_Day2

--- Week3/Day1/sample7.cpp
+++ Week3/Day1/sample7.cpp
@@ -1,6 +1,17 @@
 #include <iostream>
 #include <vector>
+#include <option>
 
+/*
+    Developer part : what is the API? What is the design? What is the corner case:
+        Scenario 1: My Input has atleast 1 value
+                    Some of the values in the input are odd
+        Scenario 2: My input has atleast 1 value . None of the values in the input are odd.
+        Scenario 3: Input is empty:
+                    Nothing Will work! [ Not my concern. Input Invalid Exception]
+
+*/
+// if initializer syntax
 void FindOddNumbers( std::vector<int>&ans,const std::vector<int>&result){
     for(const int &val:result){
         if(val%2){
@@ -12,7 +23,7 @@ void FindOddNumbers( std::vector<int>&ans,const std::vector<int>&result){
     
 }
 
-std::vector<int> FindOddNumbers( const std::vector<int>&ans){
+std::optional<std::vector<int>> FindOddNumbers( const std::vector<int>&ans){
     if(ans.size()==0)
         throw std::runtime_error("Empty Container\n");
     
@@ -23,21 +34,48 @@ std::vector<int> FindOddNumbers( const std::vector<int>&ans){
             res.push_back(val);
         }
     }
-    if(res.size()==0)
-        throw std::runtime_error("No odd number found\n");
-    
+    if(result.empty())
+        return std::nullopt;
+
+    return res;
 }
 
 int main(int argc, char const *argv[])
 {
     // if - initializer syntax
     
-    std::vector<int> in{10, 20, 30, 40, 41, 51, 97};
-    if (std::vector<int> result = FindOddNumbers(in);!result.empty()){
-        for(int i:result){
-            std::cout << i << "\n";
-        }
+    std::vector<int> in{10, 20, 30, 40, 50, 60, 90};
+    // if (std::vector<int> result = FindOddNumbers(in);!result.empty()){
+    //     for(int i:result){
+    //         std::cout << i << "\n";
+    //     }
+    // }
+    std::optional<std::vector<int>> result;
+    try
+    {
+        result = FindOddNumbers(in);
+    }
+    catch(const std::exception& e)
+    {
+        std::cerr << e.what() << '\n';
+    }
+    
+    if (result.has_value()){
+        std::cout << result.value()[0];
     }
 
-        return 0;
+    return 0;
 }
+
+
+
+/*
+    std::optional : Indicates an alegebruc data type that may 
+                    - either contain a value 
+                    - or a symbol indicating "ABSENCE OF INFORMATION"
+
+*/
+/*
+    FindOdd numbers is a function that optionally return a std::vector<int> OR
+    Find Odd Numbers: may or may not return a result
+*/
---
+++ Week3/Day2/Sample1/demo1.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+//TAX PAYERS: EMPLOYEES, BUSINESSOWNERS
+
+
+int main(int argc, char const *argv[])
+{
+    
+    return 0;
+}
